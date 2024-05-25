846bda8a83cdbb86a3b93c8ef35322bc162d651c
Nash Stewart <nashkstewart@gmail.com>
2024-05-24 22:34:08 -0600

    Add submission for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  let oldestYear = person.yearOfDeath;
+  if (person.yearOfDeath == undefined) { oldestYear = new Date().getFullYear(); }
+  return oldestYear - person.yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+  return people.sort((a, b) => getAge(b) - getAge(a))[0];
 };
 
 // Do not edit below this line
