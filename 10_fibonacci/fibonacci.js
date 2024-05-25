c7f0c5fab7cf35dd7992690bef2097ebf55eec96
Nash Stewart <nashkstewart@gmail.com>
2024-05-24 21:39:40 -0600

    Add submission for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  if (position < 0) return 'OOPS'; 
+  if (position == 0) return 0;
 
+  let prev1 = 0;
+  let prev2 = 1;
+  for (let i = 2; i <= position; i++) {
+    const temp = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = temp;
+  }
+  return prev2;
 };
 
 // Do not edit below this line
