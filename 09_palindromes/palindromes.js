d6784d1dbf7ff246f0247b09d4dcf9ae3ae8e665
Nash Stewart <nashkstewart@gmail.com>
2024-05-24 20:57:14 -0600

    Add submission for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const nonAlphaNumeric = /[^a-z0-9]/gi;
+  const alphaNumeric = string.replace(nonAlphaNumeric, '').toLowerCase();
+  const alphaNumericReversed = alphaNumeric
+    .split('')
+    .reverse()
+    .join('');
+ 
+  return alphaNumeric == alphaNumericReversed;
 };
 
 // Do not edit below this line
